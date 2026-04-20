f8fe7cb1b915d1310263f93351f2d1265e8a8b12
Xawier Słupik <48760619+xBlixon@users.noreply.github.com>
2026-04-20 20:15:17 +0200

    Simple scene zoom for further game debugging

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include <cstdint>
+#include <concepts>
 
 #include "imgui.h"
 #include "imgui-SFML.h"
@@ -11,8 +12,13 @@
 
 int main() {
     ImVec4 color(0.f, 0.8f, 0.f, 1.f);
+
     sf::RenderWindow window(sf::VideoMode({ 640, 480 }), "ImGui + SFML = <3");
     window.setFramerateLimit(60);
+
+	sf::View view(sf::FloatRect({ 0.f, 0.f }, { 640.f, 480.f }));
+	window.setView(view);
+
     ImGui::SFML::Init(window);
 
     sf::CircleShape shape(100.f);
@@ -25,7 +31,29 @@ int main() {
             if (event->is<sf::Event::Closed>()) {
                 window.close();
             }
+            else if (const auto* scrolled = event->getIf<sf::Event::MouseWheelScrolled>()) {
+                if (ImGui::GetIO().WantCaptureMouse) continue;
+
+                bool ctrlPressed = sf::Keyboard::isKeyPressed(sf::Keyboard::Key::LControl) ||
+                    sf::Keyboard::isKeyPressed(sf::Keyboard::Key::RControl);
+
+                if (ctrlPressed) {
+					// Zoom is counter-intuitive.
+                    // Zoom means how much is the view scaled.
+                    // If zoom is 0.9 it means the view gets smaller,
+                    // so the world looks bigger.
+                    if (scrolled->delta > 0) {
+                        view.zoom(0.9f);
+                    }
+                    else {
+                        view.zoom(1.1f);
+                    }
+
+                    window.setView(view);
+                }
+            }
         }
+        window.clear();
 
         ImGui::SFML::Update(window, deltaClock.restart());
 
@@ -35,11 +63,9 @@ int main() {
         ImGui::ColorEdit3("MyColor##1", (float*)&color);
         ImGui::End();
 
-        window.clear();
-
         shape.setFillColor(ISConvert::imVec4ToSfColor(color));
-
         window.draw(shape);
+
         ImGui::SFML::Render(window);
         window.display();
     }
